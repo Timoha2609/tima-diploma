aa2f754468b6c2bb88b9dec5fb1cc00c260ec9cd
Темирлан Карипжанов <@itstep.loc>
2023-10-13 21:45:53 +0600

    user

---
+++ src/components/layouts/NavbarComponents.vue
@@ -0,0 +1,19 @@
+
+
+<template>
+  
+ <div class="side"> 
+<p-button v-if="!user" @click="googleRegister" label="Войти" icon="pi pi-sign-in"/> 
+<p-button v-if="!user" @click="googleLogout" label="Выйти" icon="pi-sign-out"/> 
+ </div>
+
+</template>
+
+<script setup>
+import PButton from 'primevue/button'
+import {useUser} from '@/composables/useUser'
+
+const {user,googleRegister,googleLogout} =useUser()
+
+
+</script>
