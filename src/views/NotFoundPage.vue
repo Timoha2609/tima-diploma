e3b9fd179a4717c33b340f5af3d04d064fd3ff78
Темирлан Карипжанов <@itstep.loc>
2023-11-01 21:13:51 +0600

    anim142242

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -1,10 +1,21 @@
 <template>
-    Страница не найдена
+   <div class="slova"> 
+    <p> 
+        Бывают ошибки в жизни, но не стоит сдаваться, продолжай свой путь. А это хоть и 404, но ничего, все будет хорошо, продолжай двигаться вперед. 
+    </p>
+   </div>
   </template>
   
   <script setup>
   </script>
   
   <style scoped>
+
+  .slova{
+    text-align: center;
+    font-style: italic;
+    font-family: sans-serif;
+  }
+
 
   </style>
